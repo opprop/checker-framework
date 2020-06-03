cd3eeb52b5c0b3f6fec7d731c67ba2b6706c9e6c
d367wang <d367wang@uwaterloo.ca>
2020-06-03 01:32:19 +0000

    add test cases

---
+++ checker/tests/nullness/flow/Issue2076.java
@@ -0,0 +1,29 @@
+// Test case for issue #2076:
+// https://github.com/typetools/checker-framework/issues/2076
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.dataflow.qual.ThrowsException;
+
+public class Issue2076 {
+    private @Nullable Object mObj = null;
+
+    @ThrowsException(NullPointerException.class)
+    public void buildAndThrow() {}
+
+    public @NonNull Object m1() {
+        if (mObj == null) {
+            buildAndThrow();
+        }
+        return mObj;
+    }
+
+    public void m2(@Nullable Object obj) {
+        int n;
+        if (obj == null) {
+            buildAndThrow();
+        } else {
+            n = obj.hashCode();
+        }
+    }
+}

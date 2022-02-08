7a46743442c6d1ee3c2144a9a30330273e414f58
zcai <z44cai@uwaterloo.ca>
2022-02-08 12:16:17 -0500

    Fix visitor rule for invalid polymorphic qualifier use (#204)

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -720,7 +720,7 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
             }
 
             for (AnnotationMirror poly : polys) {
-                if (type.hasAnnotationRelaxed(poly)) {
+                if (type.hasAnnotation(poly)) {
                     return Collections.singletonList(
                             new DiagMessage(Kind.ERROR, "invalid.polymorphic.qualifier.use", poly));
                 }
